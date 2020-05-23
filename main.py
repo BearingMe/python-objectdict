05f5fe5731d6930de32cf53599ec15afd857cf80
Bruno Gomes <49632633+BearingMe@users.noreply.github.com>
2020-05-23 19:21:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,3 @@ class object_dict(dict):
 		json_object = json.dumps(self, indent=2)
 		with open(filename, mode) as file:
 			file.write(json_object)
-
-
-carro = object_dict({
-	'nome': 'Civic',
-	'marca': 'Honda'
-})	
-
-carro.ano = 2019
-carro.preco = 98900
-
-carro.show_data = lambda self: print(self)
-carro.show_data()
